62a6db9109fce4cfbed36eaccdca9326de683211
Kirill Pekarskiy <kipe122006@gmail.com>
2024-04-11 20:07:57 +0300

    Создание страницы регистрации и входа в аккаунт

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+require("./gulp/dev.js");
+
+gulp.task("default", gulp.series(
+    "clean:dev",
+     gulp.parallel("html:dev", "scss:dev", "images:dev", "icons:dev", "js:dev"),
+     gulp.parallel("server:dev", "watch:dev")
+));
